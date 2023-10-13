152813d8f641495ea80aa0212df074e0e8c33881
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-10-13 06:12:44 +0100

    added entry model

--- recipe_log/admin.py
+++ recipe_log/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Recipe_name, Entry
+
+admin.site.register(Recipe_name)
+admin.site.register(Entry)
--- recipe_log/models.py
+++ recipe_log/models.py
@@ -2,5 +2,24 @@ from django.db import models
 
 # Create your models here
 
-dir(models)
-help(models)
+class Recipe_name(models.Model):
+    """The name of the recipe the user entered"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+   
+    def __str__(self):
+        """Return the string representation of the model"""
+        return self.text
+    
+class Entry(models.Model):
+    """The conntent of a recipe"""
+    recipe = models.ForeignKey(Recipe_name, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Returns the string representaion of the entry"""
+        return f"{self.text[:50]}..."
