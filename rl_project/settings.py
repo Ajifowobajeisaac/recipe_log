a81527c3b0865ef57dc9f705d2e8f431600093ea
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-11-16 06:09:52 +0000

    set Debug to false on live site

--- rl_project/settings.py
+++ rl_project/settings.py
@@ -144,6 +144,7 @@ from platformshconfig import Config
 config = Config()
 if config.is_valid_platform():
     ALLOWED_HOSTS.append('.platformsh.site')
+    DEBUG = False
 
     if config.appDir:
         STATIC_ROOT = Path(config.appDir) / 'static'
